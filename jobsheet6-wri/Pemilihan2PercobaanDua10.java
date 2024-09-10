a5f042dd8aedf290b75d6979611531600199dedf
rahma <fijriatirahmaturrizqi@gmail.com>
2024-09-10 23:49:24 +0700

    cek jenis segitiga

--- jobsheet6-wri/Pemilihan2PercobaanDua10.java
+++ jobsheet6-wri/Pemilihan2PercobaanDua10.java
@@ -5,20 +5,26 @@ public class Pemilihan2PercobaanDua10 {
         Scanner input10 = new Scanner(System.in);
         
         double sudut1, sudut2, sudut3, totalSudut;
-        System.out.println("masukkan sudut pertama");
+        System.out.print("masukkan sudut pertama: ");
         sudut1 = input10.nextDouble();
-        System.out.println("masukkan sudut kedua");
+        System.out.print("masukkan sudut kedua: ");
         sudut2 = input10.nextDouble();
-        System.out.println("masukkan sudut ketiga");
+        System.out.print("masukkan sudut ketiga: ");
         sudut3 = input10.nextDouble();
 
         totalSudut = sudut1 + sudut2 + sudut3;
 
         if(totalSudut == 180){
-            if((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90)){
+            if ((sudut1 == sudut2) && (sudut1 == sudut3)) {
+                System.out.println("segitiga sama sisi");
+            }
+            else if((sudut1 == sudut2) || (sudut1 == sudut3) || (sudut2 == sudut3)){
+                System.out.println("segitiga sama kaki");
+            }
+            else if((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90)){
                 System.out.println("segitiga siku siku");
             }else{
-                System.out.println("bukan segitiga siku-siku");
+                System.out.println("segitiga sembarang");
             }
         }else{
             System.out.println("bukan segitiga");
